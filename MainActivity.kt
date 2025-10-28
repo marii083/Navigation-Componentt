ec749f6ac62548c172dd518a62df169786b2d41d
Marianny <mga5@discente.ifpe.edu.br>
2025-10-28 18:16:33 -0300

    Remove comment from MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,18 @@
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+            
+        navController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController
+    }
+}
